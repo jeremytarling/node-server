e0661efc28f01395ea69d43af666a677988d4e0a
Jeremy Tarling <jeremy.tarling@gmail.com>
2012-07-29 21:29:13 +0000

    added request handlers for text post requests

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,19 +1,29 @@
-var exec = require("child_process").exec;
+var querystring = require("querystring");
 
-function start(response) {
+function start(response, postData) {
   console.log("Request handler 'start' was called.");
+
+  var body = '<html>\n'+
+    '<head>\n'+
+    '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />\n'+ 
+    '</head>\n'+
+    '<body>\n'+
+    '<form action="/upload" method="post">\n'+
+    '<textarea name="text" rows="10" cols="60"></textarea><br />\n'+
+    '<input type="submit" value="Submit text" />\n'+
+    '</form>\n'+
+    '</body>\n'+
+    '</html>\n';
   
-  exec("ls -lah", function (error, stdout, stderr) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write(stdout);
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
     response.end();
-  });
 }
 
-function upload(response) {
+function upload(response, postData) {
   console.log("Request handler 'upload' was called.");
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello Upload");
+  response.write("You've sent the text: " + querystring.parse(postData).text);
   response.end();
 }
 
--- server.js
+++ server.js
@@ -2,18 +2,25 @@ var http = require("http");
 var url = require("url");
 
 function start(route, handle) {
+  
   function onRequest(request, response) {
+    var postData = "";
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
     
-    route(handle, pathname, response);
+    request.setEncoding("utf8");
+
+    request.addListener("data", function(postDataChunk) {
+      postData += postDataChunk;
+      console.log("Received POST data chunk '"+ postDataChunk + "'.");
+    });
+
+    request.addListener("end", function() {
+      route(handle, pathname, response, postData);
+    });
+
   }
-  /*
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello from node.js!");
-    response.end();
-  }
-  */
+  
   http.createServer(onRequest).listen(8888);
   console.log("Server running on port 8888.");
 }
